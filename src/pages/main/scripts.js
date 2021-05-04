0316dd65f2f6dbdd986363c4dd0cc034b98e04be
IKateU <ieu.tlt@mail.ru>
2021-05-04 10:01:29 +0300

    Переписала разметку на БЭМ, добавила новые компоненты, переменные, миксину и transition

--- src/pages/main/scripts.js
+++ src/pages/main/scripts.js
@@ -1,25 +1,25 @@
-var popupBtn = document.querySelector('.slider-js');
-var popupForm = document.querySelector('.popup-form');
-var popupOverlay = document.querySelector('.popup-overlay');
-var popupClose = document.querySelector('.popup-close');
+var popupBtn = document.querySelector('.slider__js');
+var popupForm = document.querySelector('.popup__form');
+var popupOverlay = document.querySelector('.popup__overlay');
+var popupClose = document.querySelector('.popup__close');
 var input = popupForm.querySelector('input');
 
 popupBtn.addEventListener('click', function() {
-    popupForm.classList.add('popup-open'); 
-    popupOverlay.classList.add('popup-overlay-open');
+    popupForm.classList.add('popup__open'); 
+    popupOverlay.classList.add('popup__overlay-open');
     input.focus();
 })
 
 popupClose.addEventListener('click', function() {
-    popupForm.classList.remove('popup-open'); 
-    popupOverlay.classList.remove('popup-overlay-open');
+    popupForm.classList.remove('popup__open'); 
+    popupOverlay.classList.remove('popup__overlay-open');
 })
 
 window.addEventListener('keydown', function(evt) {
     if(evt.keyCode === 27) {
-        if(popupForm.classList.contains('popup-open')) {
-            popupForm.classList.remove('popup-open'); 
-            popupOverlay.classList.remove('popup-overlay-open');    
+        if(popupForm.classList.contains('popup__open')) {
+            popupForm.classList.remove('popup__open'); 
+            popupOverlay.classList.remove('popup__overlay-open');    
         }
     }
 })
